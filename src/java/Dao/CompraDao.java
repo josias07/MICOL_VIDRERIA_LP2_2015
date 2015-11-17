a8e6015ba1e6dce34b20996cd6afe6f7e246df1b
GISS <GISS@Giss>
2015-11-17 12:50:02 -0500

    se creo la interface clienteDao

--- src/java/Dao/CompraDao.java
+++ src/java/Dao/CompraDao.java
@@ -1,13 +1,13 @@
 
 package Dao;
 
-import Beans.Compra;
+import Beans.Compra;    
 import java.util.List;
 
 public interface CompraDao {
     
     public boolean agregarCompra(Compra compra);
-        public boolean eliminarcompra(int id_compra);
+    public boolean eliminarcompra(int id_compra);
     public boolean actualizarcompra(Compra compra);
     public List<Compra> listarcompra();
 }
---
+++ web/registrarventas.jsp
@@ -0,0 +1,5 @@
+
+
+<%@include file="WEB-INF/fragmentos/topadm.jspf"%>
+
+<%@include file="WEB-INF/fragmentos/bottom.jspf"%>
